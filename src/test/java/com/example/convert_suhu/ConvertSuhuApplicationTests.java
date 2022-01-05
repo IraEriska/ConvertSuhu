b7c0f4fe3d1971a31a3bc9ec401ecb557e78a106
IraEriska <ira009155@gmail.com>
2022-01-05 23:39:46 +0700

    suhu

---
+++ src/test/java/com/example/convert_suhu/ConvertSuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convert_suhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertSuhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
